1279c28b9f648b8f8beb2c017f2bb2eb11ba750b
David Allison <62114487+david-allison@users.noreply.github.com>
2025-03-29 03:43:06 +0000

    Doc/copy docs from fsrs-rs
    Some references are updated to use TypeScript-based syntax/method references

--- index.d.ts
+++ index.d.ts
@@ -16,20 +16,63 @@ export interface ModelEvaluation {
   rmseBins: number
 }
 export declare class FSRS {
+  /**
+   * - Parameters must be provided before running commands that need them.
+   * - Parameters may be an empty array to use the default values instead.
+   */
   constructor(parameters?: Array<number> | undefined | null)
+  /** Calculate appropriate parameters for the provided review history. */
   computeParameters(trainSet: Array<FSRSItem>, enableShortTerm: boolean, progressJsFn?: (err: null | Error, value: { current: number, total: number, percent: number }) => void, timeout?: number): Promise<Array<number>>
+  /**
+   * The intervals and memory states for each answer button.
+   *
+   * Parameters must have been provided when calling [`new FSRS()`]{@link constructor}.
+   */
   nextStates(currentMemoryState: MemoryState | undefined | null, desiredRetention: number, daysElapsed: number): NextStates
   benchmark(trainSet: Array<FSRSItem>, enableShortTerm: boolean): Array<number>
+  /**
+   * Determine how well the model and parameters predict performance.
+   *
+   * Parameters must have been provided when calling [`new FSRS()`]{@link constructor}.
+   */
   evaluate(trainSet: Array<FSRSItem>): ModelEvaluation
+  /**
+   * If a card has incomplete learning history, memory state can be approximated from
+   * current sm2 values.
+   *
+   * Parameters must have been provided when calling [`new FSRS()`]{@link constructor}.
+   */
   memoryStateFromSm2(easeFactor: number, interval: number, sm2Retention: number): MemoryState
+  /**
+   * Calculate the current memory state for a given card's history of reviews.
+   * In the case of truncated reviews, `startingState` can be set to the value of
+   * {@link memoryStateFromSm2} for the first review (which should not be included
+   * in {@link FSRSItem}). If not provided, the card starts as new.
+   *
+   * Parameters must have been provided when calling [`new FSRS()`]{@link constructor}.
+   */
   memoryState(item: FSRSItem, startingState?: MemoryState | undefined | null): MemoryState
 }
 export declare class FSRSReview {
   constructor(rating: number, deltaT: number)
+  /** 1-4 */
   get rating(): number
+  /**
+   * The number of days that passed
+   * # Warning
+   * `delta_t` for item first(initial) review must be 0
+   */
   get deltaT(): number
   toJSON(): string
 }
+/**
+ * Stores a list of reviews for a card, in chronological order. Each FSRSItem corresponds
+ * to a single review, but contains the previous reviews of the card as well, after the
+ * first one.
+ *
+ * When used during review, the last item should include the correct `delta_t`, but
+ * the provided rating is ignored as all four ratings are returned by `.nextStates()`
+ */
 export declare class FSRSItem {
   constructor(reviews: Array<FSRSReview>)
   get reviews(): Array<FSRSReview>
